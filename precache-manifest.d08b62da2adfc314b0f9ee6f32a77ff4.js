97b66ff35c1ee0ede52e3863ce59d05128908d86
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-06-16 18:18:40 -0700

    Updates

---
+++ precache-manifest.d08b62da2adfc314b0f9ee6f32a77ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89897cad1c81fa08a6c2d7333ca47183",
+    "url": "/Film-Project/index.html"
+  },
+  {
+    "revision": "81262bb4be72df0a0387",
+    "url": "/Film-Project/static/css/main.c9c92d0e.chunk.css"
+  },
+  {
+    "revision": "9904a8d03d0839597fc5",
+    "url": "/Film-Project/static/js/2.809b9a50.chunk.js"
+  },
+  {
+    "revision": "81262bb4be72df0a0387",
+    "url": "/Film-Project/static/js/main.59fdb39f.chunk.js"
+  },
+  {
+    "revision": "b22de45f3160f93f5366",
+    "url": "/Film-Project/static/js/runtime~main.4040cd28.js"
+  }
+]);
